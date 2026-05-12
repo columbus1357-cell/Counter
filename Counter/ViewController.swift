0e9f3d8b25e50d0030983ceac2428977c521bd95
Ymir <Columbus1357@gmail.com>
2026-05-13 00:30:59 +0700

    Исправления по ревью: приватные элементы, имена, отступы

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,56 +8,52 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var historyTextView: UITextView!
-    var count = 0
-    var historyLog = "История изменений:\n"
-
-    func data() -> String {
-        let data = Date()
+    // MARK: - Outlets
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var historyTextView: UITextView!
+    // MARK: - Properties
+    private var count = 0
+    private var historyLog = "История изменений:\n"
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Private Methods
+    private func currentDateString() -> String {
+        let date = Date()
         let format = DateFormatter()
         format.dateFormat = "dd.MM.yyyy HH:mm:ss"
-        return format.string(from: data)
+        return format.string(from: date)
     }
     
-    func updateHistory() {
+    private func updateHistory() {
         historyTextView.text = historyLog
     }
-    
-    @IBAction func plusTapped(_ sender: Any) {
+    // MARK: - Actions
+    @IBAction private func plusTapped(_ sender: Any) {
         count += 1
         counterLabel.text = "Значение счётчика: \(count)"
-        historyLog += "\(data()): значение изменено на +1\n"
+        historyLog += "\(currentDateString()): значение изменено на +1\n"
         updateHistory()
     }
     
-    @IBAction func minusTapped(_ sender: Any) {
+    @IBAction private func minusTapped(_ sender: Any) {
         if count == 0 {
-            historyLog += "\(data()):попытка уменьшить счётчик ниже 0\n"
+            historyLog += "\(currentDateString()): попытка уменьшить счётчик ниже 0\n"
             updateHistory()
         } else if count > 0 {
             count -= 1
             counterLabel.text = "Значение счётчика: \(count)"
-            historyLog += "\(data()):значение изменено на -1\n"
+            historyLog += "\(currentDateString()): значение изменено на -1\n"
             updateHistory()
         }
     }
     
-    @IBAction func resetTapped(_ sender: Any) {
+    @IBAction private func resetTapped(_ sender: Any) {
         count = 0
         counterLabel.text = "Значение счётчика: \(count)"
-        historyLog += "\(data()):значение сброшено\n"
+        historyLog += "\(currentDateString()): значение сброшено\n"
         updateHistory()
-        
     }
     
-   
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-
-
 }
