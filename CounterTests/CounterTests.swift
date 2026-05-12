2e7dcb79da9a994d59f518056f3f0ed424ef1d29
Ymir <Columbus1357@gmail.com>
2026-05-12 15:47:01 +0700

    Создан проект Counter

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,19 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Aleksandr on 12.05.2026.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
